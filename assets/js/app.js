6496e46ef23f6ad8b072598d9c8e31038f12285c
TomasGieco <tomas.gieco@hotmail.com>
2021-12-20 20:51:01 -0300

    form update

--- assets/js/app.js
+++ assets/js/app.js
@@ -75,7 +75,6 @@ tabs.forEach(tab => {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-
 function scrollActive() {
     const scrollY = window.pageYOffset
 
@@ -140,3 +139,41 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*==================== FORM ====================*/
+window.addEventListener("DOMContentLoaded", () => {
+    var form = document.getElementById("form");
+    var status = document.getElementById("status");
+
+    function success() {
+        form.reset();
+        status.innerHTML = "Thanks!";
+        status.classList.add("success");
+    }
+
+    function error() {
+        status.classList.add("error");
+        status.innerHTML = "Oops, There was a problem!";
+    }
+
+    form.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
